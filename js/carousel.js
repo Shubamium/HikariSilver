ae34fdf6f7d4e94e95dc3d542e23141972104901
Vincent Liussyaputra <liusvinv@gmail.com>
2023-05-20 23:06:42 +0700

    Add carousel script

---
+++ js/carousel.js
@@ -0,0 +1,22 @@
+let carouselImgEl = document.getElementById('carousel-img');
+
+let currentImgId = 0;
+
+let imageList = [
+    'https://i.ibb.co/X8qXSTD/fanart1-min-1.png',
+    'assets/replaceable/schedules.webp',
+]
+
+
+function changeCarousel(input){
+    currentImgId += input;
+    // Clamp the value
+    let res = Math.abs(currentImgId % imageList.length);
+
+    console.log(res);
+    updateCarouselImg(res);
+}
+
+function updateCarouselImg(index){
+    carouselImgEl.setAttribute('src',imageList[index]);
+}
